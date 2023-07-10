82e55e445d17d3ce8a3e11aabf034359d1660cf5
Pasha Khomchenko <pavel.kh005@gmail.com>
2023-07-10 15:13:16 -0700

    remove streaming mode

--- src/BeatNet/BeatNet.py
+++ src/BeatNet/BeatNet.py
@@ -11,7 +11,6 @@ from BeatNet.log_spect import LOG_SPECT
 import librosa
 import sys
 from BeatNet.model import BDA
-import pyaudio
 import matplotlib.pyplot as plt
 import time
 import threading
@@ -26,8 +25,7 @@ class BeatNet:
         ----------
         Inputs: 
             model: An scalar in the range [1,3] to select which pre-trained CRNN models to utilize. 
-            mode: An string to determine the working mode. i.e. 'stream', 'realtime', 'online' and ''offline.
-                'stream' mode: Uses the system microphone to capture sound and does the process in real-time. Due to training the model on standard mastered songs, it is highly recommended to make sure the microphone sound is as loud as possible. Less reverbrations leads to the better results.  
+            mode: An string to determine the working mode. i.e. 'realtime', 'online' and ''offline. 
                 'Realtime' mode: Reads an audio file chunk by chunk, and processes each chunck at the time.
                 'Online' mode: Reads the whole audio and feeds it into the BeatNet CRNN at the same time and then infers the parameters on interest using particle filtering.
                 'offline' mode: Reads the whole audio and feeds it into the BeatNet CRNN at the same time and then inferes the parameters on interest using madmom dynamic Bayesian network. This method is quicker that madmom beat/downbeat tracking.
@@ -79,35 +77,13 @@ class BeatNet:
         else:
             raise RuntimeError(f'Failed to open the trained model: {model}')
         self.model.eval()
-        if self.mode == 'stream':
-            self.stream_window = np.zeros(self.log_spec_win_length + 2 * self.log_spec_hop_length, dtype=np.float32)                                          
-            self.stream = pyaudio.PyAudio().open(format=pyaudio.paFloat32,
-                                             channels=1,
-                                             rate=self.sample_rate,
-                                             input=True,
-                                             frames_per_buffer=self.log_spec_hop_length,)
                                              
-    def process(self, audio_path=None):   
-        if self.mode == "stream":
-            if self.inference_model != "PF":
-                    raise RuntimeError('The infernece model should be set to "PF" for the streaming mode!')
-            self.counter = 0
-            while self.stream.is_active():
-                self.activation_extractor_stream()  # Using BeatNet causal Neural network streaming mode to extract activations
-                if self.thread:
-                    x = threading.Thread(target=self.estimator.process, args=(self.pred), daemon=True)   # Processing the inference in another thread 
-                    x.start()
-                    x.join()    
-                else:
-                    output = self.estimator.process(self.pred)
-                self.counter += 1
-
-                
-        elif self.mode == "realtime":
+    def process(self, audio_path=None):                   
+        if self.mode == "realtime":
             self.counter = 0
             self.completed = 0
             if self.inference_model != "PF":
-                raise RuntimeError('The infernece model for the streaming mode should be set to "PF".')
+                raise RuntimeError('The infernece model for the realtime mode should be set to "PF".')
             if isinstance(audio_path, str) or audio_path.all()!=None:
                 while self.completed == 0:
                     self.activation_extractor_realtime(audio_path) # Using BeatNet causal Neural network realtime mode to extract activations
@@ -146,27 +122,6 @@ class BeatNet:
         
                 else:
                     raise RuntimeError('An audio object or file directory is required for the offline usage!')
-                
-
-    def activation_extractor_stream(self):
-        # TODO: 
-        ''' Streaming window
-        Given the training input window's origin set to center, this streaming data formation causes 0.084 (s) delay compared to the trained model that needs to be fixed. 
-        '''
-        with torch.no_grad():
-            hop = self.stream.read(self.log_spec_hop_length)
-            hop = np.frombuffer(hop, dtype=np.float32)
-            self.stream_window = np.append(self.stream_window[self.log_spec_hop_length:], hop)
-            if self.counter < 5:
-                self.pred = np.zeros([1,2])
-            else:
-                feats = self.proc.process_audio(self.stream_window).T[-1]
-                feats = torch.from_numpy(feats)
-                feats = feats.unsqueeze(0).unsqueeze(0).to(self.device)
-                pred = self.model(feats)[0]
-                pred = self.model.final_pred(pred)
-                pred = pred.cpu().detach().numpy()
-                self.pred = np.transpose(pred[:2, :])
 
 
     def activation_extractor_realtime(self, audio_path):
